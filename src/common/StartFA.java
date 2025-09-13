c2b73c12c313ebb4ed397c25f09a6ed8ca27beea
cerebrummi <info@birkeheeren.de>
2025-09-13 21:09:55 +0200

    export SFA result as file

--- src/common/StartFA.java
+++ src/common/StartFA.java
@@ -1,22 +1,56 @@
 package common;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.StringJoiner;
+
 public class StartFA
 {
-  final static int NUMBER_OF_STEPS = 17;
-  
+   final static int NUMBER_OF_STEPS = 19;
+
    public static void main(String[] args)
    {
       SFA sfa = new SFA();
-      sfa.printWalksets();
-      
-      for( int i = 0 ; i < NUMBER_OF_STEPS; i++)
+
+      try
       {
-         System.out.println("========== step start ==========");
-         sfa.step();
-         //sfa.printWalksets();
-         System.out.println(sfa.getStepNumber());
-         System.out.println("========== step end ===========");
+         File file = new File("resultFA_19only.txt");
+
+         FileOutputStream stream = new FileOutputStream(file);
+         OutputStreamWriter writer = new OutputStreamWriter(stream,
+               StandardCharsets.UTF_8);
+         StringJoiner joiner = new StringJoiner("\n");
+
+         for (int i = 0; i < NUMBER_OF_STEPS; i++)
+         {
+            sfa.step();
+            if(i == 19)
+            {
+               joiner.add("=============== step number = " + sfa.getStepNumber() +" ==========================");
+               joiner.merge(sfa.toStringJoiner());
+            }
+         }
+
+         writer.write(joiner.toString());
+         writer.flush();
+         writer.close();
+      }
+      catch (UnsupportedEncodingException e)
+      {
+         e.printStackTrace();
+      }
+      catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      }
+      catch (IOException e)
+      {
+         e.printStackTrace();
       }
    }
 }
-
--- src/module-info.java
+++ src/module-info.java
@@ -5,10 +5,7 @@
  * 
  */
 module fractalalgorithm
-{
-   requires javafx.graphics;
-   requires javafx.controls;
-   
+{  
    exports common;
    exports enums;
 }
